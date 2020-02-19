f5419ddebe0d9000316e094714f995cd88cb1c26
Domeshwar <domeshwarsn91@gmail.com>
2020-02-19 23:38:23 +0530

    Update DB.java

--- src/DB.java
+++ src/DB.java
@@ -7,7 +7,7 @@ public class DB {
 		try{
 			Class.forName("com.mysql.jdbc.Driver");
 			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/test","","");
-			system.out.println("hello how are you11222")
+			system.out.println("hello how are you11222 WE WILL DO IT")
 		}catch(Exception e){System.out.println(e);}
 		return con;
 	}
